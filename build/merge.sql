47c2e7c42b892ebca6c029164a8db62b85d1d4ff
Pau Ramos <p.ramos@unistra.fr>
2023-06-20 16:09:43 +0900

    Mergin only unique sources of each catalogue
    In principle, there are:
    2MASS: Total - unique in 2xS - unique in 2xV + unique in 2xVxS = 1248334 - 834671 - 950105 + 783987 = 247,545
    VVV: Total - unique in 2xV - unique in VxS + unique in 2xVxS = 7984472 - 972914 - 6204158 + 795450 = 1,602,850
    SIRIUS: Total - unique 2xS - unique VxS + unique 2xVxS = 8866366 - 850017 - 6385764 + 796499 = 2,427,084
    Grand total of: 247,545 + 1,602,850 + 2,427,084 = 4,277,479

--- build/merge.sql
+++ build/merge.sql
@@ -1,193 +1,5 @@
-CREATE TEMP VIEW tmass_hw AS
-  SELECT
-    *,
-    0.7829*phot_j_mag + 0.2171*phot_h_mag
-    - 0.0323*(phot_j_mag-phot_h_mag)^2
-    AS phot_hw_mag,
-    sqrt(0.035^2
-     + (0.7829*phot_j_mag_error)^2
-     + (0.2171*phot_h_mag_error)^2
-     + (0.0323*2*(phot_j_mag-phot_h_mag)*phot_j_mag_error)^2
-     + (0.0323*2*(phot_j_mag-phot_h_mag)*phot_h_mag_error)^2)
-    AS phot_hw_mag_error
-  FROM
-    tmass_sources;
-
-CREATE TEMP VIEW sirius_hw AS
-  SELECT
-    *,
-    0.7796*phot_j_mag + 0.2204*phot_h_mag
-    - 0.0326*(phot_j_mag-phot_h_mag)^2
-    AS phot_hw_mag,
-    sqrt(0.063^2
-     + (0.7796*phot_j_mag_error)^2
-     + (0.2204*phot_h_mag_error)^2
-     + (0.0326*2*(phot_j_mag-phot_h_mag)*phot_j_mag_error)^2
-     + (0.0326*2*(phot_j_mag-phot_h_mag)*phot_h_mag_error)^2)
-    AS phot_hw_mag_error
-  FROM
-    sirius_sources;
-
-CREATE TEMP TABLE vvv_hw AS
-  SELECT
-    *,
-    0.7988*phot_j_mag + 0.2012*phot_h_mag
-    - 0.0315*(phot_j_mag-phot_h_mag)^2
-    AS phot_hw_mag,
-    sqrt(0.033^2
-     + (0.7988*phot_j_mag_error)^2
-     + (0.2012*phot_h_mag_error)^2
-     + (0.0315*2*(phot_j_mag-phot_h_mag)*phot_j_mag_error)^2
-     + (0.0315*2*(phot_j_mag-phot_h_mag)*phot_h_mag_error)^2)
-    AS phot_hw_mag_error
-  FROM
-    vvv_sources
-  WHERE
-    phot_j_flag = 0 AND phot_h_flag = 0;
-
-CREATE INDEX IF NOT EXISTS temp_vvv_hw_glonglat
-  ON vvv_hw (q3c_ang2ipix(glon,glat));
-CLUSTER temp_vvv_hw_glonglat ON vvv_hw;
-ANALYZE vvv_hw;
-
-
-CREATE TEMP TABLE temp_merged_sources (
-  source_id          BIGSERIAL PRIMARY KEY,
-  tmass_source_id    BIGINT,
-  sirius_source_id   BIGINT,
-  glon               FLOAT,
-  glat               FLOAT,
-  ra                 FLOAT,
-  dec                FLOAT,
-  position_source    VARCHAR(1),
-  phot_hw_mag        FLOAT,
-  phot_hw_mag_error  FLOAT,
-  phot_hw_mag_source VARCHAR(1),
-  phot_j_mag         FLOAT,
-  phot_j_mag_error   FLOAT,
-  phot_j_mag_source  VARCHAR(1),
-  phot_h_mag         FLOAT,
-  phot_h_mag_error   FLOAT,
-  phot_h_mag_source  VARCHAR(1),
-  phot_ks_mag        FLOAT,
-  phot_ks_mag_error  FLOAT,
-  phot_ks_mag_source VARCHAR(1)
-);
-
-INSERT INTO temp_merged_sources
-SELECT
-  nextval('temp_merged_sources_source_id_seq') AS source_id,
-  t.source_id AS tmass_source_id,
-  s.source_id AS sirius_source_id,
-  wrap(COALESCE(s.glon,t.glon)) AS glon,
-  COALESCE(s.glat,t.glat) AS glat,
-  COALESCE(s.ra,t.ra) AS ra,
-  COALESCE(s.dec,t.dec) AS dec,
-  ifthenelse(s.source_id,'S','2') AS position_source,
-  select_better(
-    s.phot_hw_mag,s.phot_hw_mag_error,
-    t.phot_hw_mag,t.phot_hw_mag_error) AS phot_hw_mag,
-  select_better(
-    s.phot_hw_mag_error,s.phot_hw_mag_error,
-    t.phot_hw_mag_error,t.phot_hw_mag_error) AS phot_hw_mag_error,
-  select_char(
-    s.phot_hw_mag_error,t.phot_hw_mag_error,'S','2') AS phot_hw_mag_source,
-  select_better(
-    s.phot_j_mag,s.phot_j_mag_error,
-    t.phot_j_mag,t.phot_j_mag_error) AS phot_j_mag,
-  select_better(
-    s.phot_j_mag_error,s.phot_j_mag_error,
-    t.phot_j_mag_error,t.phot_j_mag_error) AS phot_j_mag_error,
-  select_char(
-    s.phot_j_mag_error,t.phot_j_mag_error,'S','2') AS phot_j_mag_source,
-  select_better(
-    s.phot_h_mag,s.phot_h_mag_error,
-    t.phot_h_mag,t.phot_h_mag_error) AS phot_h_mag,
-  select_better(
-    s.phot_h_mag_error,s.phot_h_mag_error,
-    t.phot_h_mag_error,t.phot_h_mag_error) AS phot_h_mag_error,
-  select_char(
-    s.phot_h_mag_error,t.phot_h_mag_error,'S','2') AS phot_h_mag_source,
-  select_better(
-    s.phot_ks_mag,s.phot_ks_mag_error,
-    t.phot_ks_mag,t.phot_ks_mag_error) AS phot_ks_mag,
-  select_better(
-    s.phot_ks_mag_error,s.phot_ks_mag_error,
-    t.phot_ks_mag_error,t.phot_ks_mag_error) AS phot_ks_mag_error,
-  select_char(
-    s.phot_ks_mag_error,t.phot_ks_mag_error,'S','2') AS phot_ks_mag_source
-FROM
-  tmass_hw AS t
-LEFT JOIN
-  sirius_hw AS s
-ON
-  q3c_join(t.glon,t.glat,s.glon,s.glat,1./3600.)
-  AND jhk_match(
-    t.phot_j_mag,s.phot_j_mag,t.phot_h_mag,s.phot_h_mag,
-    t.phot_ks_mag,s.phot_ks_mag,2.0::FLOAT)
-UNION
-SELECT
-  nextval('temp_merged_sources_source_id_seq') AS source_id,
-  t.source_id AS tmass_source_id,
-  s.source_id AS sirius_source_id,
-  wrap(COALESCE(s.glon,t.glon)) AS glon,
-  COALESCE(s.glat,t.glat) AS glat,
-  COALESCE(s.ra,t.ra) AS ra,
-  COALESCE(s.dec,t.dec) AS dec,
-  ifthenelse(s.source_id,'S','2') AS position_source,
-  select_better(
-    s.phot_hw_mag,s.phot_hw_mag_error,
-    t.phot_hw_mag,t.phot_hw_mag_error) AS phot_hw_mag,
-  select_better(
-    s.phot_hw_mag_error,s.phot_hw_mag_error,
-    t.phot_hw_mag_error,t.phot_hw_mag_error) AS phot_hw_mag_error,
-  select_char(
-    s.phot_hw_mag_error,t.phot_hw_mag_error,'S','2') AS phot_hw_mag_source,
-  select_better(
-    s.phot_j_mag,s.phot_j_mag_error,
-    t.phot_j_mag,t.phot_j_mag_error) AS phot_j_mag,
-  select_better(
-    s.phot_j_mag_error,s.phot_j_mag_error,
-    t.phot_j_mag_error,t.phot_j_mag_error) AS phot_j_mag_error,
-  select_char(
-    s.phot_j_mag_error,t.phot_j_mag_error,'S','2') AS phot_j_mag_source,
-  select_better(
-    s.phot_h_mag,s.phot_h_mag_error,
-    t.phot_h_mag,t.phot_h_mag_error) AS phot_h_mag,
-  select_better(
-    s.phot_h_mag_error,s.phot_h_mag_error,
-    t.phot_h_mag_error,t.phot_h_mag_error) AS phot_h_mag_error,
-  select_char(
-    s.phot_h_mag_error,t.phot_h_mag_error,'S','2') AS phot_h_mag_source,
-  select_better(
-    s.phot_ks_mag,s.phot_ks_mag_error,
-    t.phot_ks_mag,t.phot_ks_mag_error) AS phot_ks_mag,
-  select_better(
-    s.phot_ks_mag_error,s.phot_ks_mag_error,
-    t.phot_ks_mag_error,t.phot_ks_mag_error) AS phot_ks_mag_error,
-  select_char(
-    s.phot_ks_mag_error,t.phot_ks_mag_error,'S','2') AS phot_ks_mag_source
-FROM
-  sirius_hw AS s
-LEFT JOIN
-  tmass_hw AS t
-ON
-  q3c_join(s.glon,s.glat,t.glon,t.glat,1./3600.)
-  AND jhk_match(
-    t.phot_j_mag,s.phot_j_mag,t.phot_h_mag,s.phot_h_mag,
-    t.phot_ks_mag,s.phot_ks_mag,2.0::FLOAT)
-WHERE
-  t.source_id IS NULL;
-
-
-CREATE INDEX IF NOT EXISTS temp_merged_sources_glonglat
-  ON temp_merged_sources (q3c_ang2ipix(glon,glat));
-CLUSTER temp_merged_sources_glonglat ON temp_merged_sources;
-ANALYZE temp_merged_sources;
-
-
-DROP TABLE IF EXISTS merged_sources CASCADE;
-CREATE TABLE merged_sources (
+DROP TABLE IF EXISTS merged_sources_clean CASCADE;
+CREATE TABLE merged_sources_clean (
   source_id          BIGSERIAL PRIMARY KEY,
   tmass_source_id    BIGINT,
   sirius_source_id   BIGINT,
@@ -196,146 +8,89 @@ CREATE TABLE merged_sources (
   glat               FLOAT,
   ra                 FLOAT,
   dec                FLOAT,
-  position_source    VARCHAR(1),
+  source             VARCHAR(1),
   phot_hw_mag        FLOAT,
   phot_hw_mag_error  FLOAT,
-  phot_hw_mag_source VARCHAR(1),
   phot_j_mag         FLOAT,
   phot_j_mag_error   FLOAT,
-  phot_j_mag_source  VARCHAR(1),
   phot_h_mag         FLOAT,
   phot_h_mag_error   FLOAT,
-  phot_h_mag_source  VARCHAR(1),
   phot_ks_mag        FLOAT,
-  phot_ks_mag_error  FLOAT,
-  phot_ks_mag_source VARCHAR(1)
+  phot_ks_mag_error  FLOAT
 );
 
-ALTER TABLE merged_sources ADD CONSTRAINT
+ALTER TABLE merged_sources_clean ADD CONSTRAINT
   FK_merged_tmass_id FOREIGN KEY (tmass_source_id)
-  REFERENCES tmass_sources (source_id) ON DELETE CASCADE;
-ALTER TABLE merged_sources ADD CONSTRAINT
+  REFERENCES tmass_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE merged_sources_clean ADD CONSTRAINT
   FK_merged_sirius_id FOREIGN KEY  (sirius_source_id)
-  REFERENCES sirius_sources (source_id) ON DELETE CASCADE;
-ALTER TABLE merged_sources ADD CONSTRAINT
+  REFERENCES sirius_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE merged_sources_clean ADD CONSTRAINT
   FK_merged_vvv_id FOREIGN KEY (vvv_source_id)
-  REFERENCES vvv_sources (source_id) ON DELETE CASCADE;
+  REFERENCES vvv_sources_clean (source_id) ON DELETE CASCADE;
 
 
-INSERT INTO merged_sources
-SELECT
-  nextval('merged_sources_source_id_seq') AS source_id,
-  t.tmass_source_id AS tmass_source_id,
-  t.sirius_source_id AS sirius_source_id,
-  v.source_id AS vvv_source_id,
-  wrap(COALESCE(v.glon,t.glon)) AS glon,
-  COALESCE(v.glat,t.glat) AS glat,
-  COALESCE(v.ra,t.ra) AS ra,
-  COALESCE(v.dec,t.dec) AS dec,
-  ifthenelse(v.source_id,'V',t.position_source) AS position_source,
-  select_better(
-    v.phot_hw_mag,v.phot_hw_mag_error,
-    t.phot_hw_mag,t.phot_hw_mag_error) AS phot_hw_mag,
-  select_better(
-    v.phot_hw_mag_error,v.phot_hw_mag_error,
-    t.phot_hw_mag_error,t.phot_hw_mag_error) AS phot_hw_mag_error,
-  select_char(
-    v.phot_hw_mag_error,t.phot_hw_mag_error,
-    'V',t.phot_hw_mag_source) AS phot_hw_mag_source,
-  select_better(
-    v.phot_j_mag,v.phot_j_mag_error,
-    t.phot_j_mag,t.phot_j_mag_error) AS phot_j_mag,
-  select_better(
-    v.phot_j_mag_error,v.phot_j_mag_error,
-    t.phot_j_mag_error,t.phot_j_mag_error) AS phot_j_mag_error,
-  select_char(
-    v.phot_j_mag_error,t.phot_j_mag_error,
-    'V',t.phot_j_mag_source) AS phot_j_mag_source,
-  select_better(
-    v.phot_h_mag,v.phot_h_mag_error,
-    t.phot_h_mag,t.phot_h_mag_error) AS phot_h_mag,
-  select_better(
-    v.phot_h_mag_error,v.phot_h_mag_error,
-    t.phot_h_mag_error,t.phot_h_mag_error) AS phot_h_mag_error,
-  select_char(
-    v.phot_h_mag_error,t.phot_h_mag_error,
-    'V',t.phot_h_mag_source) AS phot_h_mag_source,
-  select_better(
-    v.phot_ks_mag,v.phot_ks_mag_error,
-    t.phot_ks_mag,t.phot_ks_mag_error) AS phot_ks_mag,
-  select_better(
-    v.phot_ks_mag_error,v.phot_ks_mag_error,
-    t.phot_ks_mag_error,t.phot_ks_mag_error) AS phot_ks_mag_error,
-  select_char(
-    v.phot_ks_mag_error,t.phot_ks_mag_error,
-    'V',t.phot_ks_mag_source) AS phot_ks_mag_source
-FROM
-  vvv_hw AS v
-LEFT JOIN
-  temp_merged_sources AS t
-ON
-  q3c_join(v.glon,v.glat,t.glon,t.glat,1./3600.)
-  AND jhk_match(
-    t.phot_j_mag,v.phot_j_mag,t.phot_h_mag,v.phot_h_mag,
-    t.phot_ks_mag,v.phot_ks_mag,2.0::FLOAT)
+INSERT INTO merged_sources_clean
+SELECT  --tmass unique sources
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  source_id AS tmass_source_id,
+  NULL AS sirius_source_id,
+  NULL AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('T' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM tmass_sources_clean as t WHERE  source_id NOT IN (SELECT tmass_source_id FROM tmass_sirius_xmatch) AND  source_id NOT IN (SELECT tmass_source_id FROM tmass_vvv_xmatch)
 UNION
-SELECT
-  nextval('merged_sources_source_id_seq') AS source_id,
-  t.tmass_source_id AS tmass_source_id,
-  t.sirius_source_id AS sirius_source_id,
-  v.source_id AS vvv_source_id,
-  wrap(COALESCE(v.glon,t.glon)) AS glon,
-  COALESCE(v.glat,t.glat) AS glat,
-  COALESCE(v.ra,t.ra) AS ra,
-  COALESCE(v.dec,t.dec) AS dec,
-  ifthenelse(v.source_id,'V',t.position_source) AS position_source,
-  select_better(
-    v.phot_hw_mag,v.phot_hw_mag_error,
-    t.phot_hw_mag,t.phot_hw_mag_error) AS phot_hw_mag,
-  select_better(
-    v.phot_hw_mag_error,v.phot_hw_mag_error,
-    t.phot_hw_mag_error,t.phot_hw_mag_error) AS phot_hw_mag_error,
-  select_char(
-    v.phot_hw_mag_error,t.phot_hw_mag_error,
-    'V',t.phot_hw_mag_source) AS phot_hw_mag_source,
-  select_better(
-    v.phot_j_mag,v.phot_j_mag_error,
-    t.phot_j_mag,t.phot_j_mag_error) AS phot_j_mag,
-  select_better(
-    v.phot_j_mag_error,v.phot_j_mag_error,
-    t.phot_j_mag_error,t.phot_j_mag_error) AS phot_j_mag_error,
-  select_char(
-    v.phot_j_mag_error,t.phot_j_mag_error,
-    'V',t.phot_j_mag_source) AS phot_j_mag_source,
-  select_better(
-    v.phot_h_mag,v.phot_h_mag_error,
-    t.phot_h_mag,t.phot_h_mag_error) AS phot_h_mag,
-  select_better(
-    v.phot_h_mag_error,v.phot_h_mag_error,
-    t.phot_h_mag_error,t.phot_h_mag_error) AS phot_h_mag_error,
-  select_char(
-    v.phot_h_mag_error,t.phot_h_mag_error,
-    'V',t.phot_h_mag_source) AS phot_h_mag_source,
-  select_better(
-    v.phot_ks_mag,v.phot_ks_mag_error,
-    t.phot_ks_mag,t.phot_ks_mag_error) AS phot_ks_mag,
-  select_better(
-    v.phot_ks_mag_error,v.phot_ks_mag_error,
-    t.phot_ks_mag_error,t.phot_ks_mag_error) AS phot_ks_mag_error,
-  select_char(
-    v.phot_ks_mag_error,t.phot_ks_mag_error,
-    'V',t.phot_ks_mag_source) AS phot_ks_mag_source
-FROM
-  temp_merged_sources AS t
-LEFT JOIN
-  vvv_hw AS v
-ON
-  q3c_join(t.glon,t.glat,v.glon,v.glat,1./3600.)
-  AND jhk_match(
-    t.phot_j_mag,v.phot_j_mag,t.phot_h_mag,v.phot_h_mag,
-    t.phot_ks_mag,v.phot_ks_mag,2.0::FLOAT)
-WHERE
-  v.source_id IS NULL;
+SELECT  --vvv unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  NULL AS tmass_source_id,
+  NULL AS sirius_source_id,
+  source_id AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('V' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM vvv_sources_clean as v WHERE  source_id NOT IN (SELECT vvv_source_id FROM vvv_sirius_xmatch) AND  source_id NOT IN (SELECT vvv_source_id FROM tmass_vvv_xmatch)
+UNION
+SELECT  --sirius unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  NULL AS tmass_source_id,
+  source_id AS sirius_source_id,
+  NULL AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('S' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM sirius_sources_clean as s WHERE  source_id NOT IN (SELECT sirius_source_id FROM vvv_sirius_xmatch) AND  source_id NOT IN (SELECT sirius_source_id FROM tmass_vvv_xmatch)
+
 
 
 CREATE INDEX IF NOT EXISTS merged_sources_radec
