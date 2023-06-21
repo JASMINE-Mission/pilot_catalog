9ca7f61931a16b05ac6db6306582710bed218dd7
Pau Ramos <p.ramos@unistra.fr>
2023-06-21 11:59:25 +0900

    Test 4
    Problems are solved, now we add all the tables together

--- build/merge.sql
+++ build/merge.sql
@@ -70,15 +70,13 @@ SELECT  --vvv unique sources
   phot_h_mag_error, 
   phot_ks_mag, 
   phot_ks_mag_error 
-  FROM vvv_sources_clean as v WHERE  v.source_id NOT IN (SELECT vvv_source_id FROM vvv_sirius_xmatch) AND  v.source_id NOT IN (SELECT vvv_source_id FROM tmass_vvv_xmatch);
-
-/*
+  FROM vvv_sources_clean as v WHERE  v.source_id NOT IN (SELECT vvv_source_id FROM vvv_sirius_xmatch) AND  v.source_id NOT IN (SELECT vvv_source_id FROM tmass_vvv_xmatch)
 UNION
 SELECT  --sirius unique sources  
   nextval('merged_sources_clean_source_id_seq') AS source_id,
-  NULL AS tmass_source_id,
+  CAST(NULL AS BIGINT) AS tmass_source_id,
   s.source_id AS sirius_source_id,
-  NULL AS vvv_source_id, 
+  CAST(NULL AS BIGINT) AS vvv_source_id, 
   compute_glon( ra, dec) as glon,
   compute_glat( ra, dec) as glat,  
   ra,  
@@ -92,8 +90,88 @@ SELECT  --sirius unique sources
   phot_h_mag_error, 
   phot_ks_mag, 
   phot_ks_mag_error 
-  FROM sirius_sources_clean as s WHERE  s.source_id NOT IN (SELECT sirius_source_id FROM vvv_sirius_xmatch) AND  s.source_id NOT IN (SELECT sirius_source_id FROM tmass_sirius_xmatch);
-*/
+  FROM sirius_sources_clean as s WHERE  s.source_id NOT IN (SELECT sirius_source_id FROM vvv_sirius_xmatch) AND  s.source_id NOT IN (SELECT sirius_source_id FROM tmass_sirius_xmatch)
+UNION
+SELECT  --sirius unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  ts.tmass_source_id AS tmass_source_id,
+  ts.sirius_source_id AS sirius_source_id,
+  CAST(NULL AS BIGINT) AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('TS' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM tmass_sirius_xmatch as ts WHERE  ts.sirius_source_id NOT IN (SELECT sirius_source_id FROM tmass_vvv_sirius_xmatch)
+UNION
+SELECT  --sirius unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  tv.tmass_source_id AS tmass_source_id,
+  CAST(NULL AS BIGINT) AS sirius_source_id,
+  tv.vvv_source_id AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('TV' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM tmass_vvv_xmatch as tv WHERE  tv.vvv_source_id NOT IN (SELECT vvv_source_id FROM tmass_vvv_sirius_xmatch)
+UNION
+SELECT  --sirius unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  CAST(NULL AS BIGINT) AS tmass_source_id,
+  vs.sirius_source_id AS sirius_source_id,
+  vs.vvv_source_id AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('VS' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM vvv_sirius_xmatch as vs WHERE  vs.sirius_source_id NOT IN (SELECT sirius_source_id FROM tmass_vvv_sirius_xmatch)
+UNION
+SELECT  --sirius unique sources  
+  nextval('merged_sources_clean_source_id_seq') AS source_id,
+  tvs.tmass_source_id AS tmass_source_id,
+  tvs.sirius_source_id AS sirius_source_id,
+  tvs.vvv_source_id AS vvv_source_id, 
+  compute_glon( ra, dec) as glon,
+  compute_glat( ra, dec) as glat,  
+  ra,  
+  dec,
+  CAST('TVS' AS VARCHAR) AS source,
+  compute_hw( phot_j_mag, phot_h_mag) AS phot_hw_mag,
+  compute_hw_error( phot_j_mag, phot_j_mag_error, phot_h_mag, phot_h_mag_error) AS phot_hw_mag_error, 
+  phot_j_mag, 
+  phot_j_mag_error, 
+  phot_h_mag, 
+  phot_h_mag_error, 
+  phot_ks_mag, 
+  phot_ks_mag_error 
+  FROM tmass_vvv_sirius_xmatch as tvs;
+
 
 
 CREATE INDEX IF NOT EXISTS merged_sources_clean_radec
