c43d3e0c5a8a28d2899f35a838251c5ece79c361
Pau Ramos <p.ramos@unistra.fr>
2023-06-21 15:03:40 +0900

    Changing the crossmatch to improve 2MASS assignations
    Many 2MASS sources are not being matched based on 2MASS wrongly? estimated magnitudes (these do not have errors). This happen to be far from the measured magnitude in the other catalogue. Instead, we make is so that the crossmatch with 2MASS uses only the measured magnitudes. This will affect ~ 100k sources, most of which only have one magnitude observed with 2MASS and are going to be matched based solely on that (on position <1", which is below 2MASS effective angular resolution anyway)

--- build/crossmatching.sql
+++ build/crossmatching.sql
@@ -9,7 +9,7 @@ weighted_avg(t.phot_ks_mag,t.phot_ks_mag_error,v.phot_ks_mag,v.phot_ks_mag_error
 select_max(t.phot_ks_mag_error,v.phot_ks_mag_error) as phot_ks_mag_error,
 t.quality_flag as tmass_quality_flag, t.rd_flg as tmass_rd_flg,
 v.phot_j_flag as vvv_phot_j_flag,v.phot_h_flag as vvv_phot_h_flag,v.phot_ks_flag as vvv_phot_ks_flag
-FROM vvv_sources_clean as v INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,v.ra,v.dec,1./3600.) AND jhk_match(t.phot_j_mag,v.phot_j_mag,t.phot_h_mag,v.phot_h_mag,t.phot_ks_mag,v.phot_ks_mag,1.0::FLOAT); 
+FROM vvv_sources_clean as v INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,v.ra,v.dec,1./3600.) AND jhk_match(CASE WHEN t.phot_j_mag_error IS NULL THEN NULL ELSE t.phot_j_mag END,v.phot_j_mag,CASE WHEN t.phot_h_mag_error IS NULL THEN NULL ELSE t.phot_h_mag END,v.phot_h_mag,CASE WHEN t.phot_ks_mag_error IS NULL THEN NULL ELSE t.phot_ks_mag END,v.phot_ks_mag,1.0::FLOAT); 
 
 CREATE INDEX IF NOT EXISTS tmass_vvv_xmatch_tmass_sourceid
 ON tmass_vvv_xmatch (tmass_source_id);
@@ -29,7 +29,7 @@ weighted_avg(t.phot_ks_mag,t.phot_ks_mag_error,s.phot_ks_mag,s.phot_ks_mag_error
 select_max(t.phot_ks_mag_error,s.phot_ks_mag_error) as phot_ks_mag_error,
 t.quality_flag as tmass_quality_flag, t.rd_flg as tmass_rd_flg,
 s.plate_name as plate_name
-FROM sirius_sources_clean as s INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,s.ra,s.dec,1./3600.) AND jhk_match(t.phot_j_mag,s.phot_j_mag,t.phot_h_mag,s.phot_h_mag,t.phot_ks_mag,s.phot_ks_mag,1.0::FLOAT); 
+FROM sirius_sources_clean as s INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,s.ra,s.dec,1./3600.) AND jhk_match(CASE WHEN t.phot_j_mag_error IS NULL THEN NULL ELSE t.phot_j_mag END,s.phot_j_mag,CASE WHEN t.phot_h_mag_error IS NULL THEN NULL ELSE t.phot_h_mag END,s.phot_h_mag,CASE WHEN t.phot_ks_mag_error IS NULL THEN NULL ELSE t.phot_ks_mag END,s.phot_ks_mag,1.0::FLOAT); 
 
 CREATE INDEX IF NOT EXISTS tmass_sirius_xmatch_tmass_sourceid
 ON tmass_sirius_xmatch (tmass_source_id);
