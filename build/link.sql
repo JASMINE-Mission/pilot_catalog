2d250894a0aa7aa956b43e2a5a1d7d738dac7be3
Pau Ramos <p.ramos@unistra.fr>
2023-09-06 15:23:54 +0900

    New link: first separately, then together
    Step 1: for now, only TMASS (official Gaia crossmatch) and SIRIUS (closest sources at 2002 epoch + 1mag diff in JHKs).
    The global crossmatch with merged sources is decided based on which of the individual crossmatches found the closests Gaia source.

--- build/link.sql
+++ build/link.sql
@@ -1,9 +1,72 @@
-DROP TABLE IF EXISTS link_gdr3 CASCADE;
-CREATE TABLE link_gdr3 (
+-- Link Gaia DR3 <-> 2MASS
+DROP TABLE IF EXISTS link_gdr3_tmass CASCADE;
+CREATE TABLE link_gdr3_tmass (
   link_id          BIGSERIAL PRIMARY KEY,
-  merged_source_id BIGINT NOT NULL,
+  tmass_source_id BIGINT NOT NULL,
   gdr3_source_id   BIGINT NOT NULL,
   distance         FLOAT(10) NOT NULL
+);
+
+INSERT INTO link_gdr3_tmass
+  (tmass_source_id,gdr3_source_id,distance)
+SELECT
+  t.source_id AS tmass_source_id,
+  g.source_id AS gdr3_source_id,
+  3600.0*q3c_dist(t.ra,t.dec,g.ra,g.dec) AS distance
+FROM gdr3_sources AS g
+JOIN tmass_sources_clean AS t
+ON g.tmass_designation = t.designation;
+
+
+-- Link Gaia DR3 <-> SIRIUS
+DROP TABLE IF EXISTS link_gdr3_sirius CASCADE;
+CREATE TABLE link_gdr3_sirius (
+  link_id          BIGSERIAL PRIMARY KEY,
+  sirius_source_id BIGINT NOT NULL,
+  gdr3_source_id   BIGINT NOT NULL,
+  distance         FLOAT(10) NOT NULL
+);
+
+INSERT INTO link_gdr3_sirius
+  (sirius_source_id,gdr3_source_id,distance)
+SELECT
+  aux.sirius_source_id AS sirius_source_id,
+  g.source_id AS gdr3_source_id,
+  aux.distance AS distance
+FROM gdr3_sources AS g,
+  LATERAL(
+  SELECT s.source_id as sirius_source_id, 3600.0*q3c_dist(s.ra,s.dec,g.ra,g.dec) as distance 
+  FROM 
+      (SELECT * FROM sirius_sources_clean AS s0 
+      WHERE jhk_match(s0.phot_j_mag,g.phot_j_mag_pred,
+                      s0.phot_h_mag,g.phot_h_mag_pred,
+                      s0.phot_ks_mag,g.phot_ks_mag_pred,1.0::FLOAT)) as s
+  WHERE q3c_join(s.ra,s.dec,g.ra,g.dec,1.0/3600.0)
+  ORDER BY distance ASC LIMIT 1) as aux;
+
+
+-- Link Gaia DR3 <-> VVV
+DROP TABLE IF EXISTS link_gdr3_vvv CASCADE;
+CREATE TABLE link_gdr3_vvv (
+  link_id          BIGSERIAL PRIMARY KEY,
+  vvv_source_id BIGINT NOT NULL,
+  gdr3_source_id   BIGINT NOT NULL,
+  distance         FLOAT(10) NOT NULL
+);
+
+-- Concatenate all
+
+DROP TABLE IF EXISTS link_gdr3 CASCADE;
+CREATE TABLE link_gdr3 (
+  link_id                BIGSERIAL PRIMARY KEY,
+  merged_source_id       BIGINT NOT NULL,
+  gdr3_source_id         BIGINT NOT NULL,
+  gdr3_tmass_source_id   BIGINT,
+  gdr3_vvv_source_id     BIGINT,
+  gdr3_sirius_source_id  BIGINT,
+  distance_tmass         FLOAT(10),
+  distance_vvv           FLOAT(10),
+  distance_sirius        FLOAT(10)
 );
 
 ALTER TABLE link_gdr3 ADD CONSTRAINT
@@ -15,33 +78,24 @@ ALTER TABLE link_gdr3 ADD CONSTRAINT
 
 
 INSERT INTO link_gdr3
-  (merged_source_id,gdr3_source_id,distance)
-SELECT
-  aux.merged_source_id,
-  g.source_id AS gdr3_source_id,
-  distance
-FROM
-  (SELECT ra,dec,source_id FROM gdr3_sources WHERE tmass_designation IS NULL) AS g
-LEFT JOIN LATERAL(
-  SELECT m.source_id as merged_source_id,q3c_dist(m.ra,m.dec,g.ra,g.dec) as distance,m.tmass_source_id FROM merged_sources AS m
-  WHERE q3c_join(g.glon,g.glat,m.glon,m.glat,1.0/3600.0)
-  ORDER BY distance ASC LIMIT 1
-) as aux
-UNION
+  (merged_source_id,gdr3_tmass_source_id,gdr3_vvv_source_id,gdr3_sirius_source_id,distance_tmass,distance_vvv,distance_sirius)
 SELECT
   m.source_id AS merged_source_id,
-  g.source_id AS gdr3_source_id,
-  3600.0*q3c_dist(m.ra,m.dec,g.ra,g.dec) AS distance
-FROM 
-  (SELECT ra,dec,source_id,tmass_designation FROM gdr3_sources WHERE tmass_designation IS NOT NULL) AS g
-JOIN
-  tmass_sources_clean AS t
-ON
-  g.tmass_designation = t.designation
-JOIN
-  (SELECT ra,dec,source_id,tmass_source_id FROM merged_sources WHERE tmass_source_id IS NOT NULL) as m
-ON
-  m.tmass_source_id = t.source_id;
+  Case When lsirius.distance <= lvvv.distance And lsirius.distance <= ltmass.distance Then lsirius.gdr3_source_id
+        When lvvv.distance < lsirius.distance And lvvv.distance <= ltmass.distance Then  lvvv.gdr3_source_id
+        Else ltmass.gdr3_source_id
+  End As gdr3_source_id
+  ltmass.gdr3_source_id AS gdr3_tmass_source_id,
+  lvvv.gdr3_source_id AS gdr3_vvv_source_id,
+  lsirius.gdr3_source_id AS gdr3_sirius_source_id,
+  ltmass.distance AS distance_tmass,
+  lvvv.distance AS distance_vvv,
+  lsirius.distance AS distance_sirius
+FROM merged_sources AS m
+  LEFT JOIN link_gdr3_tmass as ltmass on m.tmass_source_id = ltmass.tmass_source_id
+  LEFT JOIN link_gdr3_vvv as lvvv on m.vvv_source_id = lvvv.vvv_source_id
+  LEFT JOIN link_gdr3_sirius as lsirius on m.sirius_source_id = lsirius.sirius_source_id
+WHERE (link_gdr3_tmass IS NOT NULL) OR (link_gdr3_vvv IS NOT NULL) OR (link_gdr3_sirius IS NOT NULL)
 
 CREATE INDEX IF NOT EXISTS link_gdr3_merged_source_id
   ON link_gdr3 (merged_source_id);
@@ -49,3 +103,6 @@ CREATE INDEX IF NOT EXISTS link_gdr3_gdr3_source_id
   ON link_gdr3 (gdr3_source_id);
 CLUSTER link_gdr3_merged_source_id ON link_gdr3;
 ANALYZE link_gdr3;
+
+
+-- SELECT m.source_id as merged_source_id,g.source_id as gdr3_source_id,3600.*q3c_dist(m.ra,m.dec,g.ra,g.dec) as distance FROM gdr3_sources as g LEFT JOIN merged_sources as m ON q3c_join(m.ra,m.dec,g.ra,g.dec,1.0/3600.0) Do this first and then do what this comment says: https://stackoverflow.com/questions/76879521/postgresql-fast-query-to-get-most-recent-rows
