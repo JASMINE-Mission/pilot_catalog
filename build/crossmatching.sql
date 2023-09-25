edfe1aba8ffa8e5f2ffc1367e8f6bd0a2a059124
Pau Ramos <p.ramos@unistra.fr>
2023-09-25 16:07:33 +0900

    Typos

--- build/crossmatching.sql
+++ build/crossmatching.sql
@@ -31,8 +31,10 @@ weighted_avg(t.phot_h_mag,1/POWER(t.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.p
 SQRT(weighted_avg_error(1/POWER(t.phot_h_mag_error,2),1/POWER(s.phot_h_mag_error,2))) as phot_h_mag_error,
 weighted_avg(t.phot_ks_mag,1/POWER(t.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(s.phot_ks_mag_error,2)) as phot_ks_mag, 
 SQRT(weighted_avg_error(1/POWER(t.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
+t.phot_j_mag as tmass_j_mag,t.phot_h_mag as tmass_h_mag,t.phot_ks_mag as tmass_ks_mag,
 t.phot_j_mag_error as tmass_j_mag_error,t.phot_h_mag_error as tmass_h_mag_error,t.phot_ks_mag_error as tmass_ks_mag_error,
 t.quality_flag as tmass_quality_flag, t.rd_flg as tmass_rd_flg,
+s.phot_j_mag as sirius_j_mag,s.phot_h_mag as sirius_h_mag,s.phot_ks_mag as sirius_ks_mag,
 s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error,
 s.plate_name as plate_name
 FROM sirius_sources_clean as s INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,s.ra,s.dec,1./3600.) AND jhk_match(CASE WHEN t.phot_j_mag_error IS NULL THEN NULL ELSE t.phot_j_mag END,s.phot_j_mag,CASE WHEN t.phot_h_mag_error IS NULL THEN NULL ELSE t.phot_h_mag END,s.phot_h_mag,CASE WHEN t.phot_ks_mag_error IS NULL THEN NULL ELSE t.phot_ks_mag END,s.phot_ks_mag,1.0::FLOAT); 
@@ -52,8 +54,10 @@ weighted_avg(v.phot_h_mag,1/POWER(v.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.p
 SQRT(weighted_avg_error(1/POWER(v.phot_h_mag_error,2),1/POWER(s.phot_h_mag_error,2))) as phot_h_mag_error,
 weighted_avg(v.phot_ks_mag,1/POWER(v.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(s.phot_ks_mag_error,2)) as phot_ks_mag, 
 SQRT(weighted_avg_error(1/POWER(v.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
+v.phot_j_mag as vvv_j_mag,v.phot_h_mag as vvv_h_mag,v.phot_ks_mag as vvv_ks_mag,
 v.phot_j_mag_error as vvv_j_mag_error,v.phot_h_mag_error as vvv_h_mag_error,v.phot_ks_mag_error as vvv_ks_mag_error,
 v.phot_j_flag as vvv_phot_j_flag,v.phot_h_flag as vvv_phot_h_flag,v.phot_ks_flag as vvv_phot_ks_flag,
+s.phot_j_mag as sirius_j_mag,s.phot_h_mag as sirius_h_mag,s.phot_ks_mag as sirius_ks_mag,
 s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error,
 s.plate_name as plate_name
 FROM sirius_sources_clean as s INNER JOIN vvv4_sources_clean as v ON q3c_join(v.ra,v.dec,s.ra,s.dec,1./3600.) AND jhk_match(v.phot_j_mag,s.phot_j_mag,v.phot_h_mag,s.phot_h_mag,v.phot_ks_mag,s.phot_ks_mag,1.0::FLOAT); 
