1e9106e3bb122758526fc2f23c925dbe3ad88ff5
Pau Ramos <p.ramos@unistra.fr>
2023-09-26 11:38:50 +0900

    Trying a new 3way crossmatch
    It ensures that the loop closes. Only sources where all three pairwise crossmatches agree are selected for the TMASSxVVVxSIRIUS table.

--- build/crossmatching.sql
+++ build/crossmatching.sql
@@ -1,6 +1,41 @@
+-- 2MASSxVVV
 DROP TABLE IF EXISTS tmass_vvv_xmatch CASCADE;
-CREATE TABLE tmass_vvv_xmatch AS
-SELECT t.source_id as tmass_source_id,v.source_id as vvv_source_id, v.ra as ra, v.dec as dec, CAST('V' AS VARCHAR(1)) AS position_source,
+CREATE TABLE tmass_vvv_xmatch (
+    xmatch_source_id    BIGSERIAL PRIMARY KEY,
+    tmass_source_id     BIGINT NOT NULL,
+    vvv_source_id       BIGINT NOT NULL,
+    ra                  FLOAT NOT NULL,
+    dec                 FLOAT NOT NULL,
+    position_source     VARCHAR(1) NOT NULL,
+    phot_j_mag          FLOAT,
+    phot_j_mag_error    FLOAT,
+    phot_h_mag          FLOAT,
+    phot_h_mag_error    FLOAT,
+    phot_ks_mag         FLOAT,
+    phot_ks_mag_error   FLOAT,
+    tmass_j_mag         FLOAT,
+    tmass_h_mag         FLOAT,
+    tmass_ks_mag        FLOAT,
+    tmass_j_mag_error   FLOAT,
+    tmass_h_mag_error   FLOAT,
+    tmass_ks_mag_error  FLOAT,
+    vvv_j_mag           FLOAT,
+    vvv_h_mag           FLOAT,
+    vvv_ks_mag          FLOAT,
+    vvv_j_mag_error     FLOAT,
+    vvv_h_mag_error     FLOAT,
+    vvv_ks_mag_error    FLOAT
+);
+
+ALTER TABLE tmass_vvv_xmatch ADD CONSTRAINT
+  FK_tmass_vvv_xmatch_tmass_id FOREIGN KEY (tmass_source_id)
+  REFERENCES tmass_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE tmass_vvv_xmatch ADD CONSTRAINT
+  FK_tmass_vvv_xmatch_vvv_id FOREIGN KEY (vvv_source_id)
+  REFERENCES vvv4_sources_clean (source_id) ON DELETE CASCADE;
+
+INSERT INTO tmass_vvv_xmatch
+SELECT nextval('tmass_vvv_xmatch_xmatch_source_id_seq') AS xmatch_source_id,t.source_id as tmass_source_id,v.source_id as vvv_source_id, v.ra as ra, v.dec as dec, CAST('V' AS VARCHAR(1)) AS position_source,
 weighted_avg(t.phot_j_mag,1/POWER(t.phot_j_mag_error,2),v.phot_j_mag,1/POWER(v.phot_j_mag_error,2)) as phot_j_mag, 
 SQRT(weighted_avg_error(1/POWER(t.phot_j_mag_error,2),1/POWER(v.phot_j_mag_error,2))) as phot_j_mag_error,
 weighted_avg(t.phot_h_mag,1/POWER(t.phot_h_mag_error,2),v.phot_h_mag,1/POWER(v.phot_h_mag_error,2)) as phot_h_mag, 
@@ -9,10 +44,8 @@ weighted_avg(t.phot_ks_mag,1/POWER(t.phot_ks_mag_error,2),v.phot_ks_mag,1/POWER(
 SQRT(weighted_avg_error(1/POWER(t.phot_ks_mag_error,2),1/POWER(v.phot_ks_mag_error,2))) as phot_ks_mag_error,
 t.phot_j_mag as tmass_j_mag,t.phot_h_mag as tmass_h_mag,t.phot_ks_mag as tmass_ks_mag,
 t.phot_j_mag_error as tmass_j_mag_error,t.phot_h_mag_error as tmass_h_mag_error,t.phot_ks_mag_error as tmass_ks_mag_error,
-t.quality_flag as tmass_quality_flag, t.rd_flg as tmass_rd_flg,
 v.phot_j_mag as vvv_j_mag,v.phot_h_mag as vvv_h_mag,v.phot_ks_mag as vvv_ks_mag,
-v.phot_j_mag_error as vvv_j_mag_error,v.phot_h_mag_error as vvv_h_mag_error,v.phot_ks_mag_error as vvv_ks_mag_error,
-v.phot_j_flag as vvv_phot_j_flag,v.phot_h_flag as vvv_phot_h_flag,v.phot_ks_flag as vvv_phot_ks_flag
+v.phot_j_mag_error as vvv_j_mag_error,v.phot_h_mag_error as vvv_h_mag_error,v.phot_ks_mag_error as vvv_ks_mag_error
 FROM vvv4_sources_clean as v INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,v.ra,v.dec,1./3600.) AND jhk_match(CASE WHEN t.phot_j_mag_error IS NULL THEN NULL ELSE t.phot_j_mag END,v.phot_j_mag,CASE WHEN t.phot_h_mag_error IS NULL THEN NULL ELSE t.phot_h_mag END,v.phot_h_mag,CASE WHEN t.phot_ks_mag_error IS NULL THEN NULL ELSE t.phot_ks_mag END,v.phot_ks_mag,1.0::FLOAT); 
 
 CREATE INDEX IF NOT EXISTS tmass_vvv_xmatch_tmass_sourceid
@@ -21,10 +54,46 @@ CREATE INDEX IF NOT EXISTS tmass_vvv_xmatch_vvv_sourceid
 ON tmass_vvv_xmatch (vvv_source_id);
 
 
-
+-- 2MASSxSIRIUS
 DROP TABLE IF EXISTS tmass_sirius_xmatch CASCADE;
-CREATE TABLE tmass_sirius_xmatch AS
-SELECT t.source_id as tmass_source_id,s.source_id as sirius_source_id, s.ra as ra, s.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
+CREATE TABLE tmass_sirius_xmatch (
+    xmatch_source_id       BIGSERIAL PRIMARY KEY,
+    tmass_source_id        BIGINT NOT NULL,
+    sirius_source_id       BIGINT NOT NULL,
+    ra                     FLOAT NOT NULL,
+    dec                    FLOAT NOT NULL,
+    position_source        VARCHAR(1) NOT NULL,
+    phot_j_mag             FLOAT,
+    phot_j_mag_error       FLOAT,
+    phot_h_mag             FLOAT,
+    phot_h_mag_error       FLOAT,
+    phot_ks_mag            FLOAT,
+    phot_ks_mag_error      FLOAT,
+    tmass_j_mag            FLOAT,
+    tmass_h_mag            FLOAT,
+    tmass_ks_mag           FLOAT,
+    tmass_j_mag_error      FLOAT,
+    tmass_h_mag_error      FLOAT,
+    tmass_ks_mag_error     FLOAT,
+    sirius_j_mag           FLOAT,
+    sirius_h_mag           FLOAT,
+    sirius_ks_mag          FLOAT,
+    sirius_j_mag_error     FLOAT,
+    sirius_h_mag_error     FLOAT,
+    sirius_ks_mag_error    FLOAT
+);
+
+ALTER TABLE tmass_sirius_xmatch ADD CONSTRAINT
+  FK_tmass_sirius_xmatch_tmass_id FOREIGN KEY (tmass_source_id)
+  REFERENCES tmass_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE tmass_sirius_xmatch ADD CONSTRAINT
+  FK_tmass_sirius_xmatch_sirius_id FOREIGN KEY (sirius_source_id)
+  REFERENCES sirius_sources_clean (source_id) ON DELETE CASCADE;
+
+
+
+INSERT INTO tmass_sirius_xmatch
+SELECT nextval('tmass_sirius_xmatch_xmatch_source_id_seq') AS xmatch_source_id,t.source_id as tmass_source_id,s.source_id as sirius_source_id, s.ra as ra, s.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
 weighted_avg(t.phot_j_mag,1/POWER(t.phot_j_mag_error,2),s.phot_j_mag,1/POWER(s.phot_j_mag_error,2)) as phot_j_mag, 
 SQRT(weighted_avg_error(1/POWER(t.phot_j_mag_error,2),1/POWER(s.phot_j_mag_error,2))) as phot_j_mag_error,
 weighted_avg(t.phot_h_mag,1/POWER(t.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.phot_h_mag_error,2)) as phot_h_mag,
@@ -33,10 +102,8 @@ weighted_avg(t.phot_ks_mag,1/POWER(t.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(
 SQRT(weighted_avg_error(1/POWER(t.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
 t.phot_j_mag as tmass_j_mag,t.phot_h_mag as tmass_h_mag,t.phot_ks_mag as tmass_ks_mag,
 t.phot_j_mag_error as tmass_j_mag_error,t.phot_h_mag_error as tmass_h_mag_error,t.phot_ks_mag_error as tmass_ks_mag_error,
-t.quality_flag as tmass_quality_flag, t.rd_flg as tmass_rd_flg,
 s.phot_j_mag as sirius_j_mag,s.phot_h_mag as sirius_h_mag,s.phot_ks_mag as sirius_ks_mag,
-s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error,
-s.plate_name as plate_name
+s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error
 FROM sirius_sources_clean as s INNER JOIN tmass_sources_clean as t ON q3c_join(t.ra,t.dec,s.ra,s.dec,1./3600.) AND jhk_match(CASE WHEN t.phot_j_mag_error IS NULL THEN NULL ELSE t.phot_j_mag END,s.phot_j_mag,CASE WHEN t.phot_h_mag_error IS NULL THEN NULL ELSE t.phot_h_mag END,s.phot_h_mag,CASE WHEN t.phot_ks_mag_error IS NULL THEN NULL ELSE t.phot_ks_mag END,s.phot_ks_mag,1.0::FLOAT); 
 
 CREATE INDEX IF NOT EXISTS tmass_sirius_xmatch_tmass_sourceid
@@ -45,9 +112,47 @@ CREATE INDEX IF NOT EXISTS tmass_sirius_xmatch_sirius_sourceid
 ON tmass_sirius_xmatch (sirius_source_id);
 
 
+
+-- VVVxSIRIUS
 DROP TABLE IF EXISTS vvv_sirius_xmatch CASCADE;
-CREATE TABLE vvv_sirius_xmatch AS
-SELECT v.source_id as vvv_source_id,s.source_id as sirius_source_id, s.ra as ra, s.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
+CREATE TABLE vvv_sirius_xmatch (
+    xmatch_source_id       BIGSERIAL PRIMARY KEY,
+    vvv_source_id          BIGINT NOT NULL,
+    sirius_source_id       BIGINT NOT NULL,
+    ra                     FLOAT NOT NULL,
+    dec                    FLOAT NOT NULL,
+    position_source        VARCHAR(1) NOT NULL,
+    phot_j_mag             FLOAT,
+    phot_j_mag_error       FLOAT,
+    phot_h_mag             FLOAT,
+    phot_h_mag_error       FLOAT,
+    phot_ks_mag            FLOAT,
+    phot_ks_mag_error      FLOAT,
+    vvv_j_mag              FLOAT,
+    vvv_h_mag              FLOAT,
+    vvv_ks_mag             FLOAT,
+    vvv_j_mag_error        FLOAT,
+    vvv_h_mag_error        FLOAT,
+    vvv_ks_mag_error       FLOAT,
+    sirius_j_mag           FLOAT,
+    sirius_h_mag           FLOAT,
+    sirius_ks_mag          FLOAT,
+    sirius_j_mag_error     FLOAT,
+    sirius_h_mag_error     FLOAT,
+    sirius_ks_mag_error    FLOAT
+);
+
+ALTER TABLE vvv_sirius_xmatch ADD CONSTRAINT
+  FK_vvv_sirius_xmatch_vvv_id FOREIGN KEY (vvv_source_id)
+  REFERENCES vvv4_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE vvv_sirius_xmatch ADD CONSTRAINT
+  FK_vvv_sirius_xmatch_sirius_id FOREIGN KEY (sirius_source_id)
+  REFERENCES sirius_sources_clean (source_id) ON DELETE CASCADE;
+
+
+
+INSERT INTO vvv_sirius_xmatch
+SELECT nextval('vvv_sirius_xmatch_xmatch_source_id_seq') AS xmatch_source_id,v.source_id as vvv_source_id,s.source_id as sirius_source_id, s.ra as ra, s.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
 weighted_avg(v.phot_j_mag,1/POWER(v.phot_j_mag_error,2),s.phot_j_mag,1/POWER(s.phot_j_mag_error,2)) as phot_j_mag, 
 SQRT(weighted_avg_error(1/POWER(v.phot_j_mag_error,2),1/POWER(s.phot_j_mag_error,2))) as phot_j_mag_error,
 weighted_avg(v.phot_h_mag,1/POWER(v.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.phot_h_mag_error,2)) as phot_h_mag,
@@ -56,10 +161,8 @@ weighted_avg(v.phot_ks_mag,1/POWER(v.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(
 SQRT(weighted_avg_error(1/POWER(v.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
 v.phot_j_mag as vvv_j_mag,v.phot_h_mag as vvv_h_mag,v.phot_ks_mag as vvv_ks_mag,
 v.phot_j_mag_error as vvv_j_mag_error,v.phot_h_mag_error as vvv_h_mag_error,v.phot_ks_mag_error as vvv_ks_mag_error,
-v.phot_j_flag as vvv_phot_j_flag,v.phot_h_flag as vvv_phot_h_flag,v.phot_ks_flag as vvv_phot_ks_flag,
 s.phot_j_mag as sirius_j_mag,s.phot_h_mag as sirius_h_mag,s.phot_ks_mag as sirius_ks_mag,
-s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error,
-s.plate_name as plate_name
+s.phot_j_mag_error as sirius_j_mag_error,s.phot_h_mag_error as sirius_h_mag_error,s.phot_ks_mag_error as sirius_ks_mag_error
 FROM sirius_sources_clean as s INNER JOIN vvv4_sources_clean as v ON q3c_join(v.ra,v.dec,s.ra,s.dec,1./3600.) AND jhk_match(v.phot_j_mag,s.phot_j_mag,v.phot_h_mag,s.phot_h_mag,v.phot_ks_mag,s.phot_ks_mag,1.0::FLOAT); 
 
 CREATE INDEX IF NOT EXISTS vvv_sirius_xmatch_vvv_sourceid
@@ -68,6 +171,8 @@ CREATE INDEX IF NOT EXISTS vvv_sirius_xmatch_sirius_sourceid
 ON vvv_sirius_xmatch (sirius_source_id);
 
 
+
+-- Three way crossmatch
 DROP TABLE IF EXISTS tmass_vvv_sirius_xmatch CASCADE;
 CREATE TABLE tmass_vvv_sirius_xmatch AS
 SELECT ts.tmass_source_id,ts.sirius_source_id,vs.vvv_source_id,ts.ra as ra, ts.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
@@ -88,61 +193,11 @@ weighted_avg3(ts.tmass_ks_mag,1/POWER(ts.tmass_ks_mag_error,2),
             vs.vvv_ks_mag,1/POWER(vs.vvv_ks_mag_error,2)) as phot_ks_mag, 
 SQRT(weighted_avg_error3(1/POWER(ts.tmass_ks_mag_error,2),
             1/POWER(ts.sirius_ks_mag_error,2),
-            1/POWER(vs.vvv_ks_mag_error,2))) as phot_ks_mag_error
-FROM vvv_sirius_xmatch as vs INNER JOIN tmass_sirius_xmatch as ts ON ts.sirius_source_id=vs.sirius_source_id;
+            1/POWER(vs.vvv_ks_mag_error,2))) as phot_ks_mag_error,
+ts.xmatch_source_id as tmass_x_sirius_id,
+vs.xmatch_source_id as vvv_x_sirius_id,
+tv.xmatch_source_id as tmass_x_vvv_id
+FROM vvv_sirius_xmatch as vs INNER JOIN tmass_sirius_xmatch as ts ON ts.sirius_source_id=vs.sirius_source_id
+INNER JOIN tmass_vvv_xmatch as tv ON tv.vvv_source_id = vs.vvv_source_id
+WHERE tv.tmass_source_id = ts.tmass_source_id;
 
-CREATE INDEX IF NOT EXISTS tmass_vvv_sirius_xmatch_tmass_sourceid
-ON tmass_vvv_sirius_xmatch (tmass_source_id);
-CREATE INDEX IF NOT EXISTS tmass_vvv_sirius_xmatch_vvv_sourceid
-ON tmass_vvv_sirius_xmatch (vvv_source_id);
-CREATE INDEX IF NOT EXISTS tmass_vvv_sirius_xmatch_sirius_sourceid
-ON tmass_vvv_sirius_xmatch (sirius_source_id);
-
-
-DROP TABLE IF EXISTS sirius_vvv_tmass_xmatch CASCADE;
-CREATE TABLE sirius_vvv_tmass_xmatch AS
-SELECT ts.tmass_source_id,ts.sirius_source_id,tv.vvv_source_id,ts.ra as ra, ts.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
-weighted_avg3(ts.tmass_j_mag,1/POWER(ts.tmass_j_mag_error,2),
-            ts.sirius_j_mag,1/POWER(ts.sirius_j_mag_error,2),
-            tv.vvv_j_mag,1/POWER(tv.vvv_j_mag_error,2)) as phot_j_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_j_mag_error,2),
-            1/POWER(ts.sirius_j_mag_error,2),
-            1/POWER(tv.vvv_j_mag_error,2))) as phot_j_mag_error,
-weighted_avg3(ts.tmass_h_mag,1/POWER(ts.tmass_h_mag_error,2),
-            ts.sirius_h_mag,1/POWER(ts.sirius_h_mag_error,2),
-            tv.vvv_h_mag,1/POWER(tv.vvv_h_mag_error,2)) as phot_h_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_h_mag_error,2),
-            1/POWER(ts.sirius_h_mag_error,2),
-            1/POWER(tv.vvv_h_mag_error,2))) as phot_h_mag_error,
-weighted_avg3(ts.tmass_ks_mag,1/POWER(ts.tmass_ks_mag_error,2),
-            ts.sirius_ks_mag,1/POWER(ts.sirius_ks_mag_error,2),
-            tv.vvv_ks_mag,1/POWER(tv.vvv_ks_mag_error,2)) as phot_ks_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_ks_mag_error,2),
-            1/POWER(ts.sirius_ks_mag_error,2),
-            1/POWER(tv.vvv_ks_mag_error,2))) as phot_ks_mag_error
-FROM tmass_sirius_xmatch as ts INNER JOIN tmass_vvv_xmatch as tv ON ts.tmass_source_id=tv.tmass_source_id;
-
-
-
-DROP TABLE IF EXISTS sirius_tmass_vvv_xmatch CASCADE;
-CREATE TABLE sirius_tmass_vvv_xmatch AS
-SELECT tv.tmass_source_id,vs.sirius_source_id,vs.vvv_source_id,vs.ra as ra, vs.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
-weighted_avg3(tv.tmass_j_mag,1/POWER(tv.tmass_j_mag_error,2),
-            vs.sirius_j_mag,1/POWER(vs.sirius_j_mag_error,2),
-            tv.vvv_j_mag,1/POWER(tv.vvv_j_mag_error,2)) as phot_j_mag, 
-SQRT(weighted_avg_error3(1/POWER(tv.tmass_j_mag_error,2),
-            1/POWER(vs.sirius_j_mag_error,2),
-            1/POWER(tv.vvv_j_mag_error,2))) as phot_j_mag_error,
-weighted_avg3(tv.tmass_h_mag,1/POWER(tv.tmass_h_mag_error,2),
-            vs.sirius_h_mag,1/POWER(vs.sirius_h_mag_error,2),
-            tv.vvv_h_mag,1/POWER(tv.vvv_h_mag_error,2)) as phot_h_mag, 
-SQRT(weighted_avg_error3(1/POWER(tv.tmass_h_mag_error,2),
-            1/POWER(vs.sirius_h_mag_error,2),
-            1/POWER(tv.vvv_h_mag_error,2))) as phot_h_mag_error,
-weighted_avg3(tv.tmass_ks_mag,1/POWER(tv.tmass_ks_mag_error,2),
-            vs.sirius_ks_mag,1/POWER(vs.sirius_ks_mag_error,2),
-            tv.vvv_ks_mag,1/POWER(tv.vvv_ks_mag_error,2)) as phot_ks_mag, 
-SQRT(weighted_avg_error3(1/POWER(tv.tmass_ks_mag_error,2),
-            1/POWER(vs.sirius_ks_mag_error,2),
-            1/POWER(tv.vvv_ks_mag_error,2))) as phot_ks_mag_error
-FROM vvv_sirius_xmatch as vs INNER JOIN tmass_vvv_xmatch as tv ON tv.vvv_source_id=vs.vvv_source_id;
