b0e47ff47ff8c21ddb2d22c1ee8b0e86220a86d3
Pau Ramos <p.ramos@unistra.fr>
2024-01-25 16:42:07 +0900

    estimate local density for all JASMINE sources
    Hw<15, looking only nearby sources up to 2mag fainter.

--- build/estimate_local_density_mergedcat.sql
+++ build/estimate_local_density_mergedcat.sql
@@ -4,7 +4,7 @@
 DROP TABLE IF EXISTS merged_sources_local_density_jasmine_2magfainter CASCADE; -- This table is the first step in the merging. It estimates de local density around each source.
 CREATE TABLE merged_sources_local_density_jasmine_2magfainter AS
 SELECT m.source_id,MIN(m.glon) glon,MIN(m.glat) glat,MIN(m.phot_hw_mag) phot_hw_mag, COUNT(radius) N, MIN(radius) Rmin, AVG(radius) Rmean, MAX(radius) Rmax, COUNT(radius)/(4*PI()*POWER(sin(MAX(radius)/3600*PI()/180/2),2))/(4.25*POWER(10,10)) AS density,MIN(hw) as min_hw_mag_neighbours,MAX(hw) as max_hw_mag_neighbours
-FROM (SELECT * FROM merged_sources WHERE phot_hw_mag<15 LIMIT 500) as m,
+FROM (SELECT * FROM merged_sources WHERE phot_hw_mag<15) as m,
   LATERAL (
     SELECT q3c_dist(m.ra,m.dec,m2.ra,m2.dec)*3600. as radius,m2.phot_hw_mag as hw FROM (SELECT source_id,ra,dec,phot_hw_mag FROM merged_sources as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:radius_threshold/3600.)) as m2 WHERE  m2.source_id!=m.source_id and m2.phot_hw_mag<=m.phot_hw_mag+:hwmagdiff_threshold
     ORDER BY radius ASC
