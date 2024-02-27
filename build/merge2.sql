626d44a2bdaef840ffb9b8eb55a87cba41dbdbdd
Pau Ramos <p.ramos@unistra.fr>
2024-02-27 17:16:42 +0900

    Cluster is a potato so need to find workaround

--- build/merge2.sql
+++ build/merge2.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS merged_sources_dups_candidates CASCADE;
 
 CREATE TABLE merged_sources_dups_candidates AS
-SELECT conf.*,m.position_source,m.magnitude_source FROM merged_sources_confusion_12_5 as conf INNER JOIN merged_sources as m on m.source_id=conf.source_id WHERE conf.count>1;
+SELECT conf.*,m.position_source,m.magnitude_source FROM (SELECT * FROM merged_sources_confusion_12_5 WHERE count>1) as conf LEFT JOIN merged_sources as m on m.source_id=conf.source_id;
 
 
 DROP TABLE IF EXISTS merged_sources_dups_typeA CASCADE;
@@ -10,7 +10,7 @@ CREATE TABLE merged_sources_dups_typeA AS --the closest neighbour is a VVV sourc
 SELECT t1.*,
 t2.source_id as source_id_neighbour,t2.position_source as position_source_neighbour,t2.ra as ra_neighbour, t2.dec as dec_neighbour 
 FROM merged_sources_dups_candidates AS t1, LATERAL(
-SELECT t2.source_id,t2.magnitude_source FROM merged_sources_dups_candidates AS t2 WHERE q3c_join(t1.ra,t1.dec,t2.ra,t2.dec,0.6/3600)
+SELECT t2.source_id,t2.magnitude_source FROM merged_sources_dups_candidates AS t2 WHERE q3c_join(t1.glon,t1.glat,t2.glon,t2.glat,0.6/3600)
 ) as aux WHERE (aux.magnitude_source=="TV" OR aux.magnitude_source=="TVS" OR aux.magnitude_source=="VS" OR aux.magnitude_source=="V") AND 
 (t1.phot_j_mag<=13 OR t1.phot_h_mag<=13 OR t1.phot_ks_mag<=13) AND (t1.magnitude_source != "V");
 
