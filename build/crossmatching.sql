3318b237257af807f943984ea8d7bc11892a5836
Pau Ramos <p.ramos@unistra.fr>
2024-03-12 14:28:29 +0900

    Ignoring VVV magnitudes for the bright

--- build/crossmatching.sql
+++ build/crossmatching.sql
@@ -35,15 +35,15 @@ ALTER TABLE tmass_vvv_xmatch ADD CONSTRAINT
   FK_tmass_vvv_xmatch_vvv_id FOREIGN KEY (vvv_source_id)
   REFERENCES vvv4_sources_clean (source_id) ON DELETE CASCADE;
 
--- Ignore any 2MASS magnitude band if it is brighter than 12 (VVV is saturated). For the bright, correct magnitudes of VVV
+-- Ignore any 2MASS magnitude band if it is brighter than 12 (VVV is saturated). For the bright, ignore magnitudes of VVV
 INSERT INTO tmass_vvv_xmatch
 SELECT nextval('tmass_vvv_xmatch_xmatch_source_id_seq') AS xmatch_source_id,t.source_id as tmass_source_id,v.source_id as vvv_source_id, v.ra as ra, v.dec as dec, CAST('V' AS VARCHAR(1)) AS position_source,
-weighted_avg(t.phot_j_mag,1/POWER(t.phot_j_mag_error,2),correct_jmag(v.phot_j_mag,t.phot_j_mag),1/POWER(v.phot_j_mag_error,2)) as phot_j_mag, 
-SQRT(weighted_avg_error(1/POWER(t.phot_j_mag_error,2),1/POWER(v.phot_j_mag_error,2))) as phot_j_mag_error,
-weighted_avg(t.phot_h_mag,1/POWER(t.phot_h_mag_error,2),correct_hmag(v.phot_h_mag,t.phot_h_mag),1/POWER(v.phot_h_mag_error,2)) as phot_h_mag, 
-SQRT(weighted_avg_error(1/POWER(t.phot_h_mag_error,2),1/POWER(v.phot_h_mag_error,2))) as phot_h_mag_error,
-weighted_avg(t.phot_ks_mag,1/POWER(t.phot_ks_mag_error,2),correct_ksmag(v.phot_ks_mag,t.phot_ks_mag),1/POWER(v.phot_ks_mag_error,2)) as phot_ks_mag, 
-SQRT(weighted_avg_error(1/POWER(t.phot_ks_mag_error,2),1/POWER(v.phot_ks_mag_error,2))) as phot_ks_mag_error,
+CASE WHEN t.phot_j_mag<=12 THEN t.phot_j_mag ELSE  weighted_avg(t.phot_j_mag,1/POWER(t.phot_j_mag_error,2),v.phot_j_mag,1/POWER(v.phot_j_mag_error,2)) as phot_j_mag, 
+CASE WHEN t.phot_j_mag<=12 THEN t.phot_j_mag_error ELSE SQRT(weighted_avg_error(1/POWER(t.phot_j_mag_error,2),1/POWER(v.phot_j_mag_error,2))) as phot_j_mag_error,
+CASE WHEN t.phot_h_mag<=12 THEN t.phot_h_mag ELSE weighted_avg(t.phot_h_mag,1/POWER(t.phot_h_mag_error,2),v.phot_h_mag,1/POWER(v.phot_h_mag_error,2)) as phot_h_mag, 
+CASE WHEN t.phot_h_mag<=12 THEN t.phot_h_mag_error ELSE SQRT(weighted_avg_error(1/POWER(t.phot_h_mag_error,2),1/POWER(v.phot_h_mag_error,2))) as phot_h_mag_error,
+CASE WHEN t.phot_ks_mag<=12 THEN t.phot_ks_mag ELSE weighted_avg(t.phot_ks_mag,1/POWER(t.phot_ks_mag_error,2),v.phot_ks_mag,1/POWER(v.phot_ks_mag_error,2)) as phot_ks_mag, 
+CASE WHEN t.phot_ks_mag<=12 THEN t.phot_ks_mag_error ELSE SQRT(weighted_avg_error(1/POWER(t.phot_ks_mag_error,2),1/POWER(v.phot_ks_mag_error,2))) as phot_ks_mag_error,
 t.phot_j_mag as tmass_j_mag,t.phot_h_mag as tmass_h_mag,t.phot_ks_mag as tmass_ks_mag,
 t.phot_j_mag_error as tmass_j_mag_error,t.phot_h_mag_error as tmass_h_mag_error,t.phot_ks_mag_error as tmass_ks_mag_error,
 v.phot_j_mag as vvv_j_mag,v.phot_h_mag as vvv_h_mag,v.phot_ks_mag as vvv_ks_mag,
@@ -156,15 +156,15 @@ ALTER TABLE vvv_sirius_xmatch ADD CONSTRAINT
   REFERENCES sirius_sources_clean (source_id) ON DELETE CASCADE;
 
 
--- Ignore any SIRIUS magnitude band if it is brighter than 12 (VVV is saturated). For the bright, correct magnitudes of VVV.
+-- Ignore any SIRIUS magnitude band if it is brighter than 12 (VVV is saturated). For the bright, ignore magnitudes of VVV.
 INSERT INTO vvv_sirius_xmatch
 SELECT nextval('vvv_sirius_xmatch_xmatch_source_id_seq') AS xmatch_source_id,v.source_id as vvv_source_id,s.source_id as sirius_source_id, s.ra as ra, s.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
-weighted_avg(correct_jmag(v.phot_j_mag,s.phot_j_mag),1/POWER(v.phot_j_mag_error,2),s.phot_j_mag,1/POWER(s.phot_j_mag_error,2)) as phot_j_mag, 
-SQRT(weighted_avg_error(1/POWER(v.phot_j_mag_error,2),1/POWER(s.phot_j_mag_error,2))) as phot_j_mag_error,
-weighted_avg(correct_hmag(v.phot_h_mag,s.phot_h_mag),1/POWER(v.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.phot_h_mag_error,2)) as phot_h_mag,
-SQRT(weighted_avg_error(1/POWER(v.phot_h_mag_error,2),1/POWER(s.phot_h_mag_error,2))) as phot_h_mag_error,
-weighted_avg(correct_ksmag(v.phot_ks_mag,s.phot_ks_mag),1/POWER(v.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(s.phot_ks_mag_error,2)) as phot_ks_mag, 
-SQRT(weighted_avg_error(1/POWER(v.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
+CASE WHEN s.phot_j_mag<=12 THEN s.phot_j_mag ELSE weighted_avg(v.phot_j_mag,1/POWER(v.phot_j_mag_error,2),s.phot_j_mag,1/POWER(s.phot_j_mag_error,2)) as phot_j_mag, 
+CASE WHEN s.phot_j_mag<=12 THEN s.phot_j_mag_error ELSE SQRT(weighted_avg_error(1/POWER(v.phot_j_mag_error,2),1/POWER(s.phot_j_mag_error,2))) as phot_j_mag_error,
+CASE WHEN s.phot_h_mag<=12 THEN s.phot_h_mag ELSE weighted_avg(v.phot_h_mag,1/POWER(v.phot_h_mag_error,2),s.phot_h_mag,1/POWER(s.phot_h_mag_error,2)) as phot_h_mag,
+CASE WHEN s.phot_h_mag<=12 THEN s.phot_h_mag_error ELSE SQRT(weighted_avg_error(1/POWER(v.phot_h_mag_error,2),1/POWER(s.phot_h_mag_error,2))) as phot_h_mag_error,
+CASE WHEN s.phot_ks_mag<=12 THEN s.phot_ks_mag ELSE weighted_avg(v.phot_ks_mag,1/POWER(v.phot_ks_mag_error,2),s.phot_ks_mag,1/POWER(s.phot_ks_mag_error,2)) as phot_ks_mag, 
+CASE WHEN s.phot_ks_mag<=12 THEN s.phot_ks_mag_error ELSE SQRT(weighted_avg_error(1/POWER(v.phot_ks_mag_error,2),1/POWER(s.phot_ks_mag_error,2))) as phot_ks_mag_error,
 v.phot_j_mag as vvv_j_mag,v.phot_h_mag as vvv_h_mag,v.phot_ks_mag as vvv_ks_mag,
 v.phot_j_mag_error as vvv_j_mag_error,v.phot_h_mag_error as vvv_h_mag_error,v.phot_ks_mag_error as vvv_ks_mag_error,
 s.phot_j_mag as sirius_j_mag,s.phot_h_mag as sirius_h_mag,s.phot_ks_mag as sirius_ks_mag,
@@ -179,28 +179,16 @@ ON vvv_sirius_xmatch (sirius_source_id);
 
 
 
--- Three way crossmatch
+-- Three way crossmatch: ignore VVV for bright mags
 DROP TABLE IF EXISTS tmass_vvv_sirius_xmatch CASCADE;
 CREATE TABLE tmass_vvv_sirius_xmatch AS
 SELECT ts.tmass_source_id,ts.sirius_source_id,vs.vvv_source_id,ts.ra as ra, ts.dec as dec, CAST('S' AS VARCHAR(1)) AS position_source,
-weighted_avg3(ts.tmass_j_mag,1/POWER(ts.tmass_j_mag_error,2),
-            ts.sirius_j_mag,1/POWER(ts.sirius_j_mag_error,2),
-            vs.vvv_j_mag,1/POWER(vs.vvv_j_mag_error,2)) as phot_j_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_j_mag_error,2),
-            1/POWER(ts.sirius_j_mag_error,2),
-            1/POWER(vs.vvv_j_mag_error,2))) as phot_j_mag_error,
-weighted_avg3(ts.tmass_h_mag,1/POWER(ts.tmass_h_mag_error,2),
-            ts.sirius_h_mag,1/POWER(ts.sirius_h_mag_error,2),
-            vs.vvv_h_mag,1/POWER(vs.vvv_h_mag_error,2)) as phot_h_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_h_mag_error,2),
-            1/POWER(ts.sirius_h_mag_error,2),
-            1/POWER(vs.vvv_h_mag_error,2))) as phot_h_mag_error,
-weighted_avg3(ts.tmass_ks_mag,1/POWER(ts.tmass_ks_mag_error,2),
-            ts.sirius_ks_mag,1/POWER(ts.sirius_ks_mag_error,2),
-            vs.vvv_ks_mag,1/POWER(vs.vvv_ks_mag_error,2)) as phot_ks_mag, 
-SQRT(weighted_avg_error3(1/POWER(ts.tmass_ks_mag_error,2),
-            1/POWER(ts.sirius_ks_mag_error,2),
-            1/POWER(vs.vvv_ks_mag_error,2))) as phot_ks_mag_error,
+CASE WHEN ts.tmass_j_mag>12 AND ts.sirius_j_mag>12 THEN weighted_avg3(ts.tmass_j_mag,1/POWER(ts.tmass_j_mag_error,2),ts.sirius_j_mag,1/POWER(ts.sirius_j_mag_error,2),vs.vvv_j_mag,1/POWER(vs.vvv_j_mag_error,2)) ELSE ts.phot_j_mag as phot_j_mag, 
+CASE WHEN ts.tmass_j_mag>12 AND ts.sirius_j_mag>12 THEN SQRT(weighted_avg_error3(1/POWER(ts.tmass_j_mag_error,2),1/POWER(ts.sirius_j_mag_error,2),1/POWER(vs.vvv_j_mag_error,2))) ELSE ts.phot_j_mag_error as phot_j_mag_error,
+CASE WHEN ts.tmass_h_mag>12 AND ts.sirius_h_mag>12 THEN weighted_avg3(ts.tmass_h_mag,1/POWER(ts.tmass_h_mag_error,2),ts.sirius_h_mag,1/POWER(ts.sirius_h_mag_error,2),vs.vvv_h_mag,1/POWER(vs.vvv_h_mag_error,2)) ELSE ts.phot_h_mag as phot_h_mag, 
+CASE WHEN ts.tmass_h_mag>12 AND ts.sirius_h_mag>12 THEN SQRT(weighted_avg_error3(1/POWER(ts.tmass_h_mag_error,2),1/POWER(ts.sirius_h_mag_error,2),1/POWER(vs.vvv_h_mag_error,2))) ELSE ts.phot_h_mag_error as phot_h_mag_error,
+CASE WHEN ts.tmass_ks_mag>12 AND ts.sirius_ks_mag>12 THEN weighted_avg3(ts.tmass_ks_mag,1/POWER(ts.tmass_ks_mag_error,2),ts.sirius_ks_mag,1/POWER(ts.sirius_ks_mag_error,2),vs.vvv_ks_mag,1/POWER(vs.vvv_ks_mag_error,2)) ELSE ts.phot_ks_mag as phot_ks_mag, 
+CASE WHEN ts.tmass_ks_mag>12 AND ts.sirius_ks_mag>12 THEN SQRT(weighted_avg_error3(1/POWER(ts.tmass_ks_mag_error,2),1/POWER(ts.sirius_ks_mag_error,2),1/POWER(vs.vvv_ks_mag_error,2))) ELSE ts.phot_ks_mag_error as phot_ks_mag_error,
 ts.xmatch_source_id as tmass_x_sirius_id,
 vs.xmatch_source_id as vvv_x_sirius_id,
 tv.xmatch_source_id as tmass_x_vvv_id
