8580b437f7154cf84247605c0d5b67d71f6e2bd4
Pau Ramos <p.ramos@unistra.fr>
2024-03-28 13:22:31 +0900

    Add confusion for clean version

--- build/estimate_confusion.sql
+++ build/estimate_confusion.sql
@@ -29,3 +29,18 @@ CREATE INDEX IF NOT EXISTS merged_sources_final_confusion_06_5_glonglat
 CLUSTER merged_sources_final_confusion_06_5_glonglat ON merged_sources_final_confusion_06_5;
 ANALYZE merged_sources_final_confusion_06_5;
 
+
+
+
+DROP TABLE IF EXISTS merged_sources_clean_confusion_06_5 CASCADE; 
+CREATE TABLE merged_sources_clean_confusion_06_5 AS
+SELECT m.source_id,COUNT(aux.sid),m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag FROM merged_sources_clean as m, LATERAL (SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag FROM merged_sources_clean as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold)) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
+
+
+-- index merged_sources_confusion_06_5 for quick access
+CREATE INDEX IF NOT EXISTS merged_sources_clean_confusion_06_5_source_id
+  ON merged_sources_clean_confusion_06_5 (source_id);
+CREATE INDEX IF NOT EXISTS merged_sources_clean_confusion_06_5_glonglat
+  ON merged_sources_clean_confusion_06_5 (q3c_ang2ipix(glon,glat));
+CLUSTER merged_sources_clean_confusion_06_5_glonglat ON merged_sources_clean_confusion_06_5;
+ANALYZE merged_sources_clean_confusion_06_5;
