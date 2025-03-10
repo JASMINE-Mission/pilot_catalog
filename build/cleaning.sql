9f7366cac66b4ed31fd93939bf8fd5927a8f66d1
Pau Ramos <p.ramos@unistra.fr>
2025-03-10 13:38:29 +0900

    More typos

--- build/cleaning.sql
+++ build/cleaning.sql
@@ -147,7 +147,7 @@ ANALYZE vvv42_full_clean_step1;
 DROP TABLE IF EXISTS vvv42_full_clean_step2 CASCADE;
 CREATE TABLE vvv42_full_clean_step2 AS
 SELECT 
-MIN(aux2.merging_id) AS source_id,AVG(aux2.ra) AS ra ,AVG(aux2.dec) AS dec, MAX(aux.Cl) as Cl, MAX(Var) AS Var,
+MIN(aux2.merging_id) AS source_id,AVG(aux2.ra) AS ra ,AVG(aux2.dec) AS dec, MAX(aux2.Cl) as Cl, MAX(aux2.Var) AS Var,
 SUM(aux2.phot_z_mag/POWER(aux2.phot_z_mag_error,2))/SUM(1/POWER(aux2.phot_z_mag_error,2)) AS phot_z_mag, 1/SQRT(SUM(1/POWER(aux2.phot_z_mag_error,2))) as phot_z_mag_error, SUM(aux2.phot_z_flag) as phot_z_flag,
 SUM(aux2.phot_y_mag/POWER(aux2.phot_y_mag_error,2))/SUM(1/POWER(aux2.phot_y_mag_error,2)) AS phot_y_mag, 1/SQRT(SUM(1/POWER(aux2.phot_y_mag_error,2))) as phot_y_mag_error, SUM(aux2.phot_y_flag) as phot_y_flag,
 SUM(aux2.phot_j_mag/POWER(aux2.phot_j_mag_error,2))/SUM(1/POWER(aux2.phot_j_mag_error,2)) AS phot_j_mag, 1/SQRT(SUM(1/POWER(aux2.phot_j_mag_error,2))) as phot_j_mag_error, SUM(aux2.phot_j_flag) as phot_j_flag,
