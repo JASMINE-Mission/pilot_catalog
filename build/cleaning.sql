99014bade7035fd5619a673a4558cc8d95f00216
Pau Ramos <p.ramos@unistra.fr>
2025-03-10 13:34:07 +0900

    Typo in description

--- build/cleaning.sql
+++ build/cleaning.sql
@@ -113,7 +113,7 @@ DROP TABLE IF EXISTS tmass_clean_step2 CASCADE;
 
 --VVV 4.2 full (with Class)
 --- keep max Var since if one of the two is variable (Var=1) then it propagates to the merged source
---- only allow merges between sources of the same class. A few percentage of sources are "probable stars (Cl=-2)" or "probable non-stellar source (Cl=-3)". This will not be merging with nearby sources that are Cl=-1 or Cl=1, respectively, which might be not ideal, but the number of occurancies is very low.
+--- only allow merges between sources of the same class. A few percentage of sources are "probable stars (Cl=-2)" or "probable non-stellar source (Cl=-3)". These will not be merging with nearby sources that are Cl=-1 or Cl=1, respectively, which might be not ideal, but the number of occurancies is very low.
 DROP TABLE IF EXISTS vvv42_full_clean_step1 CASCADE;
 CREATE TABLE vvv42_full_clean_step1 AS
 SELECT MIN(aux2.source_id) AS source_id,AVG(aux2.ra) AS ra ,AVG(aux2.dec) AS dec,MAX(aux2.Cl) as Cl,MAX(aux2.Var) AS Var,
