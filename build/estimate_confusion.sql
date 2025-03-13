9b32cc591e7bca7efad4e1cc271eab9a794ec2e1
Pau Ramos <p.ramos@unistra.fr>
2025-03-13 14:15:41 +0900

    track magnitude differences

--- build/estimate_confusion.sql
+++ build/estimate_confusion.sql
@@ -3,13 +3,16 @@
 
 DROP TABLE IF EXISTS merged_sources_confusion_06_5 CASCADE; 
 CREATE TABLE merged_sources_confusion_06_5 AS
-SELECT m.source_id,m.tmass_source_id,m.sirius_source_id,m.vvv_source_id,COUNT(aux.sid) as num_neighbours,MIN(ang_dist_mas) as min_ang_dist,MAX(ang_dist_mas) as max_ang_dist,AVG(ang_dist_mas) as avg_ang_dist,m.ra,m.dec,m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag, 
+SELECT m.source_id,m.tmass_source_id,m.sirius_source_id,m.vvv_source_id,COUNT(aux.sid) as num_neighbours,MIN(ang_dist_mas) as min_ang_dist,MAX(ang_dist_mas) as max_ang_dist,AVG(ang_dist_mas) as avg_ang_dist,m.ra,m.dec,m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag, MIN(mag_diff) as min_mag_diff_neighbours,MAX(mag_diff) as max_mag_diff_neighbours,
+SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) as phot_error,
+MIN(aux.phot_error) as min_phot_error_neighbours,
 CASE WHEN MIN(ang_dist_mas)<0.1 THEN CASE WHEN COUNT(aux.sid)=1 THEN 1 ELSE 3 END ELSE 2 END AS dups,
 select_better_agg(sid,phot_error) as best_neighbour_source_id,
-CASE WHEN MIN(phot_error)<SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) THEN 1 ELSE 0 END as select_neighbour
+CASE WHEN MIN(aux.phot_error)<SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) THEN 1 ELSE 0 END as select_neighbour
 FROM merged_sources_raw as m, LATERAL (
   SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,
   SQRT(POWER(COALESCE(m1.phot_j_mag_error,1),2)+POWER(COALESCE(m1.phot_h_mag_error,1),2)+POWER(COALESCE(m1.phot_ks_mag_error,1),2)) as phot_error, -- compute photometric error budget of the source for comparison, penalise not having a measurement
+  SQRT(POWER(COALESCE(m1.phot_j_mag_error,0)-COALESCE(m.phot_j_mag_error,0),2)+POWER(COALESCE(m1.phot_h_mag_error,0)-COALESCE(m.phot_h_mag_error,0),2)+POWER(COALESCE(m1.phot_ks_mag_error,0)-COALESCE(m.phot_ks_mag_error,0),2)) as mag_diff,
   q3c_dist(m.ra,m.dec,m1.ra,m1.dec)*3600*1000 as ang_dist_mas
   FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id
   ) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
