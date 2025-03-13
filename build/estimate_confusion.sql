4b58b264eda7fc7e98727e2fec77ccc00f2577c4
Pau Ramos <p.ramos@unistra.fr>
2025-03-13 14:55:01 +0900

    Improving neighbour selection

--- build/estimate_confusion.sql
+++ build/estimate_confusion.sql
@@ -8,11 +8,14 @@ SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1
 MIN(aux.phot_error) as min_phot_error_neighbours,
 CASE WHEN MIN(ang_dist_mas)<0.1 THEN CASE WHEN COUNT(aux.sid)=1 THEN 1 ELSE 3 END ELSE 2 END AS dups,
 select_better_agg(sid,phot_error) as best_neighbour_source_id,
-CASE WHEN MIN(aux.phot_error)<SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) THEN 1 ELSE 0 END as select_neighbour
+CASE WHEN MIN(aux.phot_error)<SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) THEN (
+  CASE WHEN (m.tmass_source_id IS NOT NULL OR SUM(has_tmass)>0) AND (MIN(brightest_mag)<LEAST(m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag)) THEN 1 ElSE 0 END -- there is a tmass source involved: if neighbour is brighter, selecte it
+) ELSE 0 END as select_neighbour
 FROM merged_sources_raw as m, LATERAL (
-  SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,
+  SELECT m1.source_id as sid,m1.tmass_source_id/m1.tmass_source_id as has_tmass,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,
   SQRT(POWER(COALESCE(m1.phot_j_mag_error,1),2)+POWER(COALESCE(m1.phot_h_mag_error,1),2)+POWER(COALESCE(m1.phot_ks_mag_error,1),2)) as phot_error, -- compute photometric error budget of the source for comparison, penalise not having a measurement
-  SQRT(POWER(COALESCE(m1.phot_j_mag_error,0)-COALESCE(m.phot_j_mag_error,0),2)+POWER(COALESCE(m1.phot_h_mag_error,0)-COALESCE(m.phot_h_mag_error,0),2)+POWER(COALESCE(m1.phot_ks_mag_error,0)-COALESCE(m.phot_ks_mag_error,0),2)) as mag_diff,
+  SQRT(POWER(COALESCE(m1.phot_j_mag,0)-COALESCE(m.phot_j_mag,0),2)+POWER(COALESCE(m1.phot_h_mag,0)-COALESCE(m.phot_h_mag,0),2)+POWER(COALESCE(m1.phot_ks_mag,0)-COALESCE(m.phot_ks_mag,0),2)) as mag_diff,
+  LEAST(m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag) as brightest_mag,
   q3c_dist(m.ra,m.dec,m1.ra,m1.dec)*3600*1000 as ang_dist_mas
   FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id
   ) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
