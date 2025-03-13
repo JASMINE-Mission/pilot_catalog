25bdd6518004d14a5cd4e7190255a74ff5efba9d
Pau Ramos <p.ramos@unistra.fr>
2025-03-13 14:06:34 +0900

    Include error budget comparison for selection
    Select best source from all of the nearby neighbours.

--- build/estimate_confusion.sql
+++ build/estimate_confusion.sql
@@ -4,9 +4,16 @@
 DROP TABLE IF EXISTS merged_sources_confusion_06_5 CASCADE; 
 CREATE TABLE merged_sources_confusion_06_5 AS
 SELECT m.source_id,m.tmass_source_id,m.sirius_source_id,m.vvv_source_id,COUNT(aux.sid) as num_neighbours,MIN(ang_dist_mas) as min_ang_dist,MAX(ang_dist_mas) as max_ang_dist,AVG(ang_dist_mas) as avg_ang_dist,m.ra,m.dec,m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag, 
-CASE WHEN MIN(ang_dist_mas)<0.1 THEN CASE WHEN COUNT(aux.sid)=1 THEN 1 ELSE 3 END ELSE 2 END AS dups
-FROM merged_sources_raw as m, LATERAL (SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,q3c_dist(m.ra,m.dec,m1.ra,m1.dec)*3600*1000 as ang_dist_mas FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
-
+CASE WHEN MIN(ang_dist_mas)<0.1 THEN CASE WHEN COUNT(aux.sid)=1 THEN 1 ELSE 3 END ELSE 2 END AS dups,
+select_better_agg(sid,phot_error) as best_neighbour_source_id,
+CASE WHEN MIN(phot_error)<SQRT(POWER(COALESCE(m.phot_j_mag_error,1),2)+POWER(COALESCE(m.phot_h_mag_error,1),2)+POWER(COALESCE(m.phot_ks_mag_error,1),2)) THEN 1 ELSE 0 END as select_neighbour
+FROM merged_sources_raw as m, LATERAL (
+  SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,
+  SQRT(POWER(COALESCE(m1.phot_j_mag_error,1),2)+POWER(COALESCE(m1.phot_h_mag_error,1),2)+POWER(COALESCE(m1.phot_ks_mag_error,1),2)) as phot_error, -- compute photometric error budget of the source for comparison, penalise not having a measurement
+  q3c_dist(m.ra,m.dec,m1.ra,m1.dec)*3600*1000 as ang_dist_mas
+  FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id
+  ) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
+--m1.phot_j_mag_error,m1.phot_h_mag_error,m1.phot_ks_mag_error,
 ALTER TABLE merged_sources_confusion_06_5 ADD CONSTRAINT
   FK_merged_tmass_id FOREIGN KEY (tmass_source_id)
   REFERENCES tmass_sources_clean (source_id) ON DELETE CASCADE;
