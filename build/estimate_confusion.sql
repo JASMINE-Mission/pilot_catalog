a94eaa2dd21a26041b7d46fd5bb2838351a7a63a
Pau Ramos <p.ramos@unistra.fr>
2025-03-13 13:54:26 +0900

    tagging duplicates based on proximity to other sources

--- build/estimate_confusion.sql
+++ build/estimate_confusion.sql
@@ -1,15 +1,25 @@
-\set angdist_threshold 0.6/3600
+\set angdist_threshold 0.25/3600 -- based on histogram of angular separations, this seems a good place to cut
 \set magdiff_threshold 5.
 
 DROP TABLE IF EXISTS merged_sources_confusion_06_5 CASCADE; 
 CREATE TABLE merged_sources_confusion_06_5 AS
-SELECT m.source_id,COUNT(aux.sid),MIN(ang_dist) as min_ang_dist,MAX(ang_dist) as max_ang_dist,AVG(ang_dist) as avg_ang_dist,m.ra,m.dec,m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag FROM merged_sources_raw as m, LATERAL (SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,q3c_dist(m.ra,m.dec,m1.ra,m1.dec) as ang_dist FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
+SELECT m.source_id,m.tmass_source_id,m.sirius_source_id,m.vvv_source_id,COUNT(aux.sid) as num_neighbours,MIN(ang_dist_mas) as min_ang_dist,MAX(ang_dist_mas) as max_ang_dist,AVG(ang_dist_mas) as avg_ang_dist,m.ra,m.dec,m.glon,m.glat,m.phot_j_mag,m.phot_h_mag,m.phot_ks_mag,m.phot_hw_mag, 
+CASE WHEN MIN(ang_dist_mas)<0.1 THEN CASE WHEN COUNT(aux.sid)=1 THEN 1 ELSE 3 END ELSE 2 END AS dups
+FROM merged_sources_raw as m, LATERAL (SELECT m1.source_id as sid,m1.glon,m1.glat,m1.phot_j_mag,m1.phot_h_mag,m1.phot_ks_mag,q3c_dist(m.ra,m.dec,m1.ra,m1.dec)*3600*1000 as ang_dist_mas FROM merged_sources_raw as m1 WHERE q3c_join(m.ra,m.dec,m1.ra,m1.dec,:angdist_threshold) AND m.source_id!=m1.source_id) aux WHERE jhk_match(aux.phot_j_mag,m.phot_j_mag,aux.phot_h_mag,m.phot_h_mag,aux.phot_ks_mag,m.phot_ks_mag,:magdiff_threshold) GROUP BY m.source_id;
 
+ALTER TABLE merged_sources_confusion_06_5 ADD CONSTRAINT
+  FK_merged_tmass_id FOREIGN KEY (tmass_source_id)
+  REFERENCES tmass_sources_clean (source_id) ON DELETE CASCADE;
+ALTER TABLE merged_sources_confusion_06_5 ADD CONSTRAINT
+  FK_merged_sirius_id FOREIGN KEY  (sirius_source_id)
+  REFERENCES sirius_sources_clean (source_id) ON DELETE CASCADE;
 
 -- index merged_sources_confusion_06_5 for quick access
 CREATE INDEX IF NOT EXISTS merged_sources_confusion_06_5_source_id
   ON merged_sources_confusion_06_5 (source_id);
 CREATE INDEX IF NOT EXISTS merged_sources_confusion_06_5_glonglat
   ON merged_sources_confusion_06_5 (q3c_ang2ipix(glon,glat));
+CREATE INDEX IF NOT EXISTS merged_sources_confusion_06_5_dupflag
+  ON merged_sources_confusion_06_5 (dups);
 CLUSTER merged_sources_confusion_06_5_glonglat ON merged_sources_confusion_06_5;
 ANALYZE merged_sources_confusion_06_5;
